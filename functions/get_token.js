466ad14ce297f427101381babc5c0b1d331abe37
Javier Alarcon <alarcon.vt@gmail.com>
2022-04-24 20:03:22 -0500

    add acces control in get token

--- functions/get_token.js
+++ functions/get_token.js
@@ -1,6 +1,14 @@
 const twilio = require('twilio')
 
 exports.handler = async function (context, event, callback) {
+  const response = new Twilio.Response();
+  // Set the CORS headers to allow Flex to make an error-free HTTP request
+  // to this Function
+  response.appendHeader('Access-Control-Allow-Origin', '*');
+  response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+
   const { ACCOUNT_SID, API_KEY_SID, API_KEY_SECRET } = context
   const accessToken = new twilio.jwt.AccessToken(ACCOUNT_SID, API_KEY_SID, API_KEY_SECRET)
   accessToken.identity = event.username
@@ -11,7 +19,10 @@ exports.handler = async function (context, event, callback) {
 
   accessToken.addGrant(grant)
 
-  callback(null, {
+  response.appendHeader('Content-Type', 'application/json');
+  response.setBody({
     token: accessToken.toJwt()
-  })
+  });
+
+  callback(null, response)
 }
