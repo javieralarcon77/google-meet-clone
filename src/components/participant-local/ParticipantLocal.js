dc0816af64e71294f543c2d308cd0c0653f370a8
Javier Alarcon <alarcon.vt@gmail.com>
2022-04-25 21:40:14 -0500

    added stop video share

--- src/components/participant-local/ParticipantLocal.js
+++ src/components/participant-local/ParticipantLocal.js
@@ -1,18 +1,29 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useContext} from 'react'
+import { RoomContext } from '../../context/room.context'
 
 export const ParticipantLocal = ({ track = undefined }) => {
   const videoContainer = useRef(null)
+  const { room } = useContext(RoomContext)
 
   useEffect(()=>{
     if(track) {
-      videoContainer.current.appendChild(track.attach())
+      if (videoContainer.current.childElementCount !== 1)
+        videoContainer.current.appendChild(track.attach())
     }
   }, [track])
 
+  const handleVideo = () => {
+    room.localParticipant.videoTracks.forEach(publication => {
+      if (publication.track.isStopped) publication.track.restart({ facingMode: 'environment' });      
+      else publication.track.stop();
+    });
+  }
+
   return (
-    <div className="participant">
+    <div className="participant" id="participant-local">
       <div>Yo</div>
       <div ref={videoContainer}></div>
+      <button onClick={handleVideo}>CAM</button>
     </div>
   )
 }
