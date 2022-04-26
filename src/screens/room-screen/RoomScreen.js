d8ba0d4c216c969422829e0629ef188d415ec871
Javier Alarcon <alarcon.vt@gmail.com>
2022-04-26 00:23:10 -0500

    update room screen

--- src/screens/room-screen/RoomScreen.js
+++ src/screens/room-screen/RoomScreen.js
@@ -1,9 +1,8 @@
-import React,{useContext} from 'react'
+import React,{ useContext, useState, useEffect, useRef } from 'react'
 
 import { RoomContext } from '../../context/room.context'
 import { useVideoLocal } from '../../hooks/useVideoLocal'
 
-import { Login } from '../../components/login/Login'
 import { ParticipantLocal } from '../../components/participant-local/ParticipantLocal'
 import { Participant } from '../../components/participant/Participant'
 
@@ -11,21 +10,73 @@ import './room-screen.css';
 
 export const RoomScreen = () => {
   const { track: trackLocal} = useVideoLocal()
-  const { connect, participants } = useContext(RoomContext)
+  const { submitConnect, participants, room } = useContext(RoomContext)
+  const [trackSelect, setTrackSelect] = useState({username: 'local', track: undefined})
+  const videoContainer = useRef(null)
+
+  useEffect(()=> {
+    if(trackSelect.track) {
+      const container = videoContainer.current
+      container.childNodes.forEach(child => { container.removeChild(child) })
+      container.appendChild(trackSelect.track.attach())
+    }
+  },[trackSelect])
+
+  useEffect(()=> {
+    initialLocalTrack()
+  },[trackLocal])
+
+  const initialLocalTrack = () => {
+    setTrackSelect({
+      username: 'local',
+      track: trackLocal,
+    })
+  }
+
+  const handleVideo = () => {
+    room.localParticipant.videoTracks.forEach(publication => {
+      if (publication.track.isStopped) publication.track.restart({ facingMode: 'environment' });      
+      else publication.track.stop();
+    });
+  }
+
+  const handleAudio = () => {
+    room.localParticipant.audioTracks.forEach(publication => {
+      if (publication.track.isStopped) publication.track.restart({ facingMode: 'environment' });      
+      else publication.track.stop();
+    });
+  }
+
+  const handleDisconnect = () => {
+    submitConnect()
+  }
+
+  const handleSelect = (track) => {
+    setTrackSelect(track)
+  }
 
   return (
     <div id="container-room">
-      <div>
-        <h1>Hola mundo!!!</h1>
-        <p>Status: {connect ? 'Conectado' : 'Desconectado'}</p>
-        <p>Count {participants.length}</p>
-        <Login />
+      <div id="video-principal">
+        <div ref={videoContainer}></div>
       </div>
       <div id='container-participant'>
-        <ParticipantLocal track={trackLocal}/>
+        <ParticipantLocal track={trackLocal} userSelect={trackSelect.username} onSelectTrack={handleSelect}/>
         {
-          participants.map((participant) => <Participant participant={participant} key={participant.sid} />)
+          participants.map((participant) => 
+            <Participant 
+              participant={participant} 
+              userSelect={trackSelect.username}
+              onSelectTrack={handleSelect}
+              key={participant.sid} 
+            />
+          )
         }
+      </div>
+      <div className="controls">
+        <button onClick={handleAudio}>MICROFONO</button>
+        <button onClick={handleVideo}>CAMARA</button>
+        <button onClick={handleDisconnect}>DISCONNECT</button>
       </div>
     </div>
   )
