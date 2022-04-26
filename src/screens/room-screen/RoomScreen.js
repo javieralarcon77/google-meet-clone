d90e112bd9de4a54a9345021ef20ad005ce92541
Javier Alarcon <alarcon.vt@gmail.com>
2022-04-25 21:38:27 -0500

    update route room screen

--- src/screens/room-screen/RoomScreen.js
+++ src/screens/room-screen/RoomScreen.js
@@ -1,25 +1,27 @@
 import React,{useContext} from 'react'
 
-import { RoomContext } from '../context/room.context'
-import { useVideoLocal } from '../hooks/useVideoLocal'
+import { RoomContext } from '../../context/room.context'
+import { useVideoLocal } from '../../hooks/useVideoLocal'
 
-import { Login } from '../components/login/Login'
-import { ParticipantLocal } from '../components/participant-local/ParticipantLocal'
-import { Participant } from '../components/participant/Participant'
+import { Login } from '../../components/login/Login'
+import { ParticipantLocal } from '../../components/participant-local/ParticipantLocal'
+import { Participant } from '../../components/participant/Participant'
+
+import './room-screen.css';
 
 export const RoomScreen = () => {
   const { track: trackLocal} = useVideoLocal()
   const { connect, participants } = useContext(RoomContext)
 
   return (
-    <div>
+    <div id="container-room">
       <div>
         <h1>Hola mundo!!!</h1>
         <p>Status: {connect ? 'Conectado' : 'Desconectado'}</p>
         <p>Count {participants.length}</p>
         <Login />
       </div>
-      <div id='container'>
+      <div id='container-participant'>
         <ParticipantLocal track={trackLocal}/>
         {
           participants.map((participant) => <Participant participant={participant} key={participant.sid} />)
