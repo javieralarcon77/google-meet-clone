e0cea5dba238d06f578c1ba1bdf4b9173183ff12
Javier Alarcon <alarcon.vt@gmail.com>
2022-04-25 21:39:47 -0500

    update username

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,19 +1,32 @@
-import React,{useState,useContext} from 'react'
+import React,{ useState, useContext, useEffect } from 'react'
 import { RoomContext } from '../../context/room.context'
 
 export const Login = () => {
-  const [username, setUsername] = useState('')
-  const { submitConnect, connect, isLoading } = useContext(RoomContext)
+  const [userNameInput, setUserNameInput] = useState('')
+  const { submitConnect, connect, isLoading, username } = useContext(RoomContext)
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    submitConnect(username)
+    submitConnect(userNameInput)
   }
+
+  useEffect(()=> {
+    setUserNameInput(username)
+  },[username])
 
   return (
     <form id="form" onSubmit={handleSubmit}>
-      Name: <input id="username" value={username} onChange={(e)=>{ setUsername(e.target.value) }} placeholder="Type your username..." required />
-      <button id="join" disabled={isLoading}>{ connect ? 'Desconectarme' : 'Conectarme' }</button>
+      Name: 
+      <input 
+        id="userNameInput" 
+        value={userNameInput} 
+        onChange={(e)=>{ setUserNameInput(e.target.value) }}
+        placeholder="Type your username..." 
+        required 
+      />
+      <button id="join" disabled={isLoading}>
+        { connect ? 'Desconectarme' : 'Conectarme' }
+      </button>
   </form>
   )
 }
