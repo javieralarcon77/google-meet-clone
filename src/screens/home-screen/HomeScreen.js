87ef26db215065ba356ee3ed5c6018902555feb7
Javier Alarcon <alarcon.vt@gmail.com>
2022-04-27 11:19:10 -0500

    implement component loading room and change styles

--- src/screens/home-screen/HomeScreen.js
+++ src/screens/home-screen/HomeScreen.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { RoomContext } from '../../context/room.context';
+
 import { Login } from '../../components/login/Login'
 import { Room } from '../../components/room/Room';
+import { LoadingRoom } from '../../components/loading-room/LoadingRoom';
+
 import './home-screen.css';
 
 export const HomeScreen = () => {
+  const { username, isLoading } = useContext(RoomContext)
+  
   return (
     <div id="container-home">
-      <h1>Enter the username</h1>
-      <Login />
-      <Room />
+      {!isLoading 
+      ? (
+        <>
+          <h1>{username === '' ? 'Enter the username' : 'Welcome to VideoChat'}</h1>
+          <Login />
+          <Room />
+        </>
+      )
+      : <LoadingRoom />
+      }
     </div>
   )
 }
